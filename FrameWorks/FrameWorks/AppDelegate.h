dd5329ed3dea06b8bcc92bba8a1e23b6ddee5c2d
Jerrywx <wangxiao117210@zongheng.com>
2016-11-16 10:00:05 +0800

    创建项目

---
+++ FrameWorks/FrameWorks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FrameWorks
+//
+//  Created by baidu on 16/11/16.
+//  Copyright © 2016年 ZongHeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
